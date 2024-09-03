d1e25efed2ae9c8cb3262d6e8b38e0a02f9267df
Danil <dposyagin@gmail.com>
2024-09-03 16:14:23 +0300

    Update ngrok url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ tg.expand();
 
 
 const Http = new XMLHttpRequest();
-const url = 'https://9893-94-250-252-189.ngrok-free.app/';
+const url = 'https://69cd-94-250-252-189.ngrok-free.app/';
 Http.open("GET", url);
 Http.setRequestHeader('ngrok-skip-browser-warning', '0');
 Http.send();
